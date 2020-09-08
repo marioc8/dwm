a3f5f500fdf200dec950e913e72f513017dffce7
marioc8 <your@email.com>
2020-09-08 22:32:55 +0200

    config update

--- dwm-6.2/config.def.h
+++ dwm-6.2/config.def.h
@@ -60,7 +60,7 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", "tmux", NULL };
 static const char scratchpadname[] = "scratchpad";
-static const char *scratchpadcmd[] = { "st", "-t", scratchpadname, "-g", "40x80", NULL };
+static const char *scratchpadcmd[] = { "st", "-t", scratchpadname, "-g", "90x50", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
